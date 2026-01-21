a41415a8bf4bba4eb51b801e37339b6ebd3011a3
lanhao945 <lanhao945@qq.com>
2026-01-21 09:42:49 +0800

    feat(init): 增加MinerUConversionResult和MinerUError到导出列表

--- text_extraction/services/__init__.py
+++ text_extraction/services/__init__.py
@@ -3,7 +3,13 @@ Service层 - 业务逻辑封装
 """
 
 from .file_storage import FileStorageService
-from .mineru_service import MinerUService
+from .mineru_service import MinerUConversionResult, MinerUError, MinerUService
 from .ontology_service import OntologyService
 
-__all__ = ["FileStorageService", "MinerUService", "OntologyService"]
+__all__ = [
+    "FileStorageService",
+    "MinerUService",
+    "MinerUConversionResult",
+    "MinerUError",
+    "OntologyService",
+]
